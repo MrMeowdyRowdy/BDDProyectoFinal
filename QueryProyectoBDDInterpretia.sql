356a22fb8401c786642604294b0317f270916d46
Dennis Ocaña <dennisocana@gmail.com>
2023-05-04 19:29:25 -0500

    Update QueryProyectoBDDInterpretia.sql
    implementacion de reglas y tipos de datos

--- QueryProyectoBDDInterpretia.sql
+++ QueryProyectoBDDInterpretia.sql
@@ -16,27 +16,89 @@ CREATE DATABASE Interpretia
 GO
 USE Interpretia
 GO
+DROP TYPE IF EXISTS mail
+DROP TYPE IF EXISTS identificacion
+DROP TYPE IF EXISTS telefono
+DROP RULE IF EXISTS mail_rule
+DROP RULE IF EXISTS identificacion_rule
+DROP RULE IF EXISTS telefono_rule
+GO
+DROP TABLE IF EXISTS Empleado
 
+GO
 -----------------------------------------------------------------
 --Creacion de regla para estructura dato mail
 -----------------------------------------------------------------
-CREATE RULE mail_ruleInterpretia
+CREATE RULE mail_rule
 AS
 @value LIKE '_%@_%._%';
 GO
 -----------------------------------------------------------------
 --Creacion de regla para estructura dato Identificacion
 -----------------------------------------------------------------
-CREATE RULE cedula_ruleInterpretia
+CREATE RULE identificacion_rule
 AS
 @value LIKE '[0-1][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' 
 OR @value LIKE '[2][0-4][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'
 OR @value LIKE '[3][0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]';
+GO
+-----------------------------------------------------------------
+--Creacion de regla para estructura dato Telefono
+-----------------------------------------------------------------
+CREATE RULE telefono_rule
+AS
+@value LIKE '[0][2-7][0-9][0-9][0-9][0-9][0-9][0-9][0-9]' 
+OR @value LIKE '[0][9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]';
+GO
+
+-----------------------------------------------------------------
+--Creacion del dato Cedula
+-----------------------------------------------------------------
+CREATE TYPE identificacion
+FROM CHAR(10);
+-----------------------------------------------------------------
+--Creacion del dato mail
+-----------------------------------------------------------------
+CREATE TYPE mail
+FROM VARCHAR(100);
+-----------------------------------------------------------------
+--Creacion del dato telefono
+-----------------------------------------------------------------
+CREATE TYPE telefono
+FROM VARCHAR(10);
+-----------------------------------------------------------------
+--Bindeo de la regla para estructura de mail al dato mail
+-----------------------------------------------------------------
+GO
+sp_bindrule mail_rule,'mail';
+GO
+-----------------------------------------------------------------
+--Bindeo de la regla para estructura de mail al dato mail
+-----------------------------------------------------------------
+GO
+sp_bindrule identificacion_rule,'identificacion';
+GO
+-----------------------------------------------------------------
+--Bindeo de la regla para estructura de mail al dato mail
+-----------------------------------------------------------------
+GO
+sp_bindrule telefono_rule,'telefono';
 GO
 
 -----------------------------------------------------------------
 --Creacion Tabla Empleado
 -----------------------------------------------------------------
 CREATE TABLE Empleado(
+CRID INT,
+nroIdentificacion IDENTIFICACION,
+sede VARCHAR(50),
+apellido NVARCHAR(30),
+nombre NVARCHAR(30),
+tlfContacto TELEFONO,
+emailPersonal MAIL,
+emailRackspace MAIL,
+fullTime BIT
 
+CONSTRAINT PK_Empleado PRIMARY KEY (CRID),
+CONSTRAINT UK_EmpleadoCRID UNIQUE (CRID),
 )
