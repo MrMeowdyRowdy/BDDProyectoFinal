298c39c5eda80c1d654bdfe47a2f6fe8a932b4f1
Dennis Ocaña <dennisocana@gmail.com>
2023-05-10 20:20:06 -0500

    Update Creación de usuarios.sql

--- Creación de usuarios.sql
+++ Creación de usuarios.sql
@@ -1,77 +1,57 @@
---  Interprete puede leer en la base de datos INTERPRETIA 
-
+-----------------------------------------------------------------
+--Creacion de logins
+-----------------------------------------------------------------
 USE [master]
 GO
-CREATE LOGIN [Interprete] WITH PASSWORD=N'12345.MonteRojo' MUST_CHANGE, DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+CREATE LOGIN [QualityAssurance] WITH PASSWORD=N'QA123.', DEFAULT_DATABASE=[Interpretia], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-use [master];
+CREATE LOGIN [LeadTeamLeader] WITH PASSWORD=N'LeadTL123.' MUST_CHANGE, DEFAULT_DATABASE=[Interpretia], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
-USE [Interpretia]
-GO
-CREATE USER [Interprete] FOR LOGIN [Interprete]
-GO
-USE [Interpretia]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [Interprete]
-GO
-USE [Interpretia]
+CREATE LOGIN [Interprete] WITH PASSWORD=N'LeadTL123.' MUST_CHANGE, DEFAULT_DATABASE=[Interpretia], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=ON, CHECK_POLICY=ON
 GO
 
-
--- LEAD Team Leader puede leer y escribir en la base de datos INTERPRETIA
-
-USE [master]
+-----------------------------------------------------------------
+--Creacion de Users - QA
+-----------------------------------------------------------------
+USE [Interpretia]
 GO
-CREATE LOGIN [Lead Team Leader] FROM WINDOWS WITH DEFAULT_DATABASE=[master]
+CREATE USER [QA1] FOR LOGIN [QualityAssurance]
 GO
-use [master];
+ALTER ROLE [Lector] ADD MEMBER [QA1]
+GO
+GRANT INSERT ON OBJECT::[dbo].[SesionQA] TO QA1
+GRANT UPDATE ON OBJECT::[dbo].[SesionQA] TO QA1
+
+-----------------------------------------------------------------
+--Creacion de Users - LTL
+-----------------------------------------------------------------
+CREATE USER [LeadTeamLeader] FOR LOGIN [LeadTeamLeader]
 GO
 USE [Interpretia]
 GO
-CREATE USER [Lead Team Leader] FOR LOGIN [Lead Team Leader]
+ALTER ROLE [Lector] ADD MEMBER [LeadTeamLeader]
 GO
-USE [Interpretia]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [Lead Team Leader]
-GO
-USE [Interpretia]
-GO
-ALTER ROLE [db_datawriter] ADD MEMBER [Lead Team Leader]
-GO
-use [master]
-GO
-GRANT CONNECT SQL TO [Lead Team Leader]
-GO
-use [master]
-GO
-GRANT VIEW ANY DEFINITION TO [Lead Team Leader]
-GO
-
--- Quality Assurance puede leer y escribir en la base de datos INTERPRETIA
-
-USE [master]
-GO
-CREATE LOGIN [Quality Assurance] WITH PASSWORD=N'12345.Interpretia', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
-GO
-USE [Interpretia]
-GO
-CREATE USER [Quality Assurance] FOR LOGIN [Quality Assurance]
-GO
-USE [Interpretia]
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [Quality Assurance]
-GO
-USE [Interpretia]
-GO
-ALTER ROLE [db_datawriter] ADD MEMBER [Quality Assurance]
-GO
-use [master]
-GO
-GRANT CONNECT SQL TO [Quality Assurance]
-GO
-
-
--- Envio de correos 
-
-msdb.dbo.sp_send_dbmail @profile_name= 'Interpretiahotmail' ,@recipients = 'chasi64.z@gmail.com'
-,@subject = 'Esto es una prueba',@body='Esto es una prueba Holaa',@body_format = 'HTML'
+--Permisos en tabla empleado
+GRANT INSERT ON OBJECT::[dbo].[Empleado] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[Empleado] TO [LeadTeamLeader]
+--Permisos en la tabla horario
+GRANT INSERT ON OBJECT::[dbo].[Horario] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[Horario] TO [LeadTeamLeader]
+--Permisos en la tabla Interprete
+GRANT INSERT ON OBJECT::[dbo].[Interprete] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[Interprete] TO [LeadTeamLeader]
+--Permisos en la tabla Interprete
+GRANT INSERT ON OBJECT::[dbo].[Llamada] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[Llamada] TO [LeadTeamLeader]
+--Permisos en la tabla OPS
+GRANT INSERT ON OBJECT::[dbo].[Operaciones] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[Operaciones] TO [LeadTeamLeader]
+--Permisos en la tabla QA
+GRANT INSERT ON OBJECT::[dbo].[QA] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[QA] TO [LeadTeamLeader]
+--Permisos en la tabla ReporteOPS
+GRANT INSERT ON OBJECT::[dbo].[ReporteOPS] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[ReporteOPS] TO [LeadTeamLeader]
+--Permisos en la tabla ReporteOPS
+GRANT INSERT ON OBJECT::[dbo].[TipoRCP] TO [LeadTeamLeader]
+GRANT UPDATE ON OBJECT::[dbo].[TipoRCP] TO [LeadTeamLeader]
